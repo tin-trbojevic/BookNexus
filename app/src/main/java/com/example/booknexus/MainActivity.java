81b8b82c9a0967bf892cf14c62baf5d5d2287123
tin-trbojevic <tintrboje@gmail.com>
2023-09-02 19:22:02 +0200

    Osnovna navigacija

--- app/src/main/java/com/example/booknexus/MainActivity.java
+++ app/src/main/java/com/example/booknexus/MainActivity.java
@@ -2,13 +2,95 @@ package com.example.booknexus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button allBooksButton, currentlyReadingBooks, alreadyReadBooksButton, wishlistBooksButton, favouritesBooksButton, aboutButton;
+
+    private boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        initViews();
+
+        allBooksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AllBooksActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        currentlyReadingBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CurrentlyReadingBooksActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        alreadyReadBooksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AlreadyReadBooksActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        favouritesBooksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FavouriteBooksActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        wishlistBooksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WishlistBooksActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+    private void initViews() {
+        allBooksButton = findViewById(R.id.all_books_button);
+        currentlyReadingBooks = findViewById(R.id.currently_reading_books_button);
+        alreadyReadBooksButton = findViewById(R.id.already_read_books_button);
+        wishlistBooksButton = findViewById(R.id.wish_list_books_button);
+        favouritesBooksButton = findViewById(R.id.favourites_books_button);
+        aboutButton = findViewById(R.id.about_button);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Press BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
